82cb4b23662d55b63b96ec3c082ce9fd83608b15
androidxiao <zhawei1993@163.com>
2018-03-31 23:29:59 +0800

    封装上传逻辑

--- latte-core/src/main/java/net/RestClient.java
+++ latte-core/src/main/java/net/RestClient.java
@@ -8,8 +8,11 @@ import net.callback.IRequest;
 import net.callback.ISuccess;
 import net.callback.RequestCallbacks;
 
+import java.io.File;
 import java.util.WeakHashMap;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -30,6 +33,7 @@ public class RestClient {
     private final IError ERROR;
     private final RequestBody BODY;
     private final LoaderStyle LOADER_STYLE;
+    private final File FILE;
     private final Context CONTEXT;
 
     public RestClient(String url,
@@ -40,6 +44,7 @@ public class RestClient {
                       IError error,
                       RequestBody body,
                       LoaderStyle loaderStyle,
+                      File file,
                       Context context) {
         this.URL = url;
         PARAMS.putAll(params);
@@ -48,8 +53,9 @@ public class RestClient {
         this.FAILURE = failure;
         this.ERROR = error;
         this.BODY = body;
-        this.LOADER_STYLE=loaderStyle;
-        this.CONTEXT=context;
+        this.LOADER_STYLE = loaderStyle;
+        this.FILE = file;
+        this.CONTEXT = context;
     }
 
     public static RestClientBuilder builder() {
@@ -65,7 +71,7 @@ public class RestClient {
         }
 
         if (LOADER_STYLE != null) {
-            LatteLoader.showLoading(CONTEXT,LOADER_STYLE);
+            LatteLoader.showLoading(CONTEXT, LOADER_STYLE);
         }
 
         switch (method) {
@@ -75,11 +81,24 @@ public class RestClient {
             case POST:
                 call = service.post(URL, PARAMS);
                 break;
+            case POST_RAW:
+                call = service.postRaw(URL, BODY);
+                break;
             case PUT:
                 call = service.put(URL, PARAMS);
                 break;
+            case PUT_RAW:
+                call = service.putRaw(URL, BODY);
+                break;
             case DELETE:
                 call = service.delete(URL, PARAMS);
+                break;
+            case UPLOAD:
+                final RequestBody requestBody =
+                        RequestBody.create(MediaType.parse(MultipartBody.FORM.toString()), FILE);
+                final MultipartBody.Part body =
+                        MultipartBody.Part.createFormData("file", FILE.getName(),requestBody);
+                call = RestCreator.getRestService().upload(URL, body);
                 break;
             default:
                 break;
@@ -90,7 +109,7 @@ public class RestClient {
         }
     }
 
-    private Callback<String> getRequestCallback(){
+    private Callback<String> getRequestCallback() {
         return new RequestCallbacks(
                 REQUEST,
                 SUCCESS,
@@ -101,20 +120,34 @@ public class RestClient {
     }
 
 
-    public final void get(){
+    public final void get() {
         request(HttpMethod.GET);
     }
 
 
-    public final void post(){
-        request(HttpMethod.POST);
+    public final void post() {
+        if (BODY == null) {
+            request(HttpMethod.POST);
+        } else {
+            if (PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.POST_RAW);
+        }
     }
 
-    public final void put(){
-        request(HttpMethod.PUT);
+    public final void put() {
+        if (BODY == null) {
+            request(HttpMethod.PUT);
+        } else {
+            if (PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.PUT_RAW);
+        }
     }
 
-    public final void delete(){
+    public final void delete() {
         request(HttpMethod.DELETE);
     }
 }
--- latte-core/src/main/java/net/RestClientBuilder.java
+++ latte-core/src/main/java/net/RestClientBuilder.java
@@ -7,6 +7,7 @@ import net.callback.IFailure;
 import net.callback.IRequest;
 import net.callback.ISuccess;
 
+import java.io.File;
 import java.util.WeakHashMap;
 
 import okhttp3.MediaType;
@@ -28,6 +29,7 @@ public class RestClientBuilder {
     private RequestBody mBody=null;
     private Context mContext=null;
     private LoaderStyle mLoaderStyle=null;
+    private File mFile=null;
 
     RestClientBuilder() {
     }
@@ -46,6 +48,17 @@ public class RestClientBuilder {
         PARAMS.put(key, value);
         return this;
     }
+
+    public final RestClientBuilder file(File file) {
+        this.mFile=file;
+        return this;
+    }
+
+    public final RestClientBuilder file(String file) {
+        this.mFile=new File(file);
+        return this;
+    }
+
 
     public final RestClientBuilder raw(String raw) {
         this.mBody = RequestBody.create(MediaType.parse("application/json;charset=UTF-8"), raw);
@@ -79,6 +92,6 @@ public class RestClientBuilder {
     }
 
     public final RestClient build() {
-        return new RestClient(mUrl, PARAMS, mRequest, mISuccess, mIFailure, mIError, mBody,mLoaderStyle,mContext);
+        return new RestClient(mUrl, PARAMS, mRequest, mISuccess, mIFailure, mIError, mBody,mLoaderStyle,mFile,mContext);
     }
 }
